6a01689758b079c442619cd312d081f7e977b0bb
Cory Remick <cory.remick@snhu.edu>
2021-10-16 14:16:51 -0700

    Add color output

--- menu.cpp
+++ menu.cpp
@@ -2,31 +2,38 @@
 
 void printMenu(ostream& outStream) {
 	// output banner
-	outStream << setfill('*') << setw(80) << "" << endl;
-	outStream << "*" << setfill(' ') << setw(26) << "" << "Chada Tech - Corner Grocer" << setw(26) << "" << "*" << endl;
-	outStream << setfill('*') << setw(80) << "" << endl;
-	outStream << "-" << setfill(' ') << setw(33) << "" << "Daily Report" << setw(33) << "" << "-" << endl;
-	outStream << setfill('-') << setw(80) << "" << endl;
+	outStream << RESET;
+	outStream << YELLOW << setfill('*') << setw(80) << "" << endl;
+	outStream << "*" << GREEN << setfill(' ') << setw(26) << "" << "Chada Tech - Corner Grocer" << setw(26) << "" << YELLOW << "*" << endl;
+	outStream << YELLOW << setfill('*') << setw(80) << "" << endl;
+	outStream << YELLOW << "-" << GREEN << setfill(' ') << setw(33) << "" << "Daily Report" << setw(33) << "" << YELLOW << "-" << endl;
+	outStream << YELLOW << setfill('-') << setw(80) << "" << endl;
 	outStream << setfill(' ') << setw(1) << endl;
 	// output menu choices
+	outStream << BLUE;
 	outStream << "1: Show Item Sales Frequency" << endl;
 	outStream << "2: Lookup Item Frequency" << endl;
 	outStream << "3: Show Item Histogram" << endl;
 	outStream << "4: Exit" << endl;
 	outStream << endl;
 	// output prompt
+	outStream << BOLDBLUE;
 	outStream << "Enter your selection as a number 1, 2, 3, or 4." << endl;
+	outStream << BOLDWHITE;
 }
 
 void handleOption(char option, istream& inStream, ostream& outStream) {
 	outStream << endl;
+	outStream << RESET;
 
 	switch (option) {
 	case '1': // Show Item Sales Frequency
 		CallProcedure("FrequencyOfItems"); // Call Python procedure to get item list and number sold.
 		break;
 	case '2': // Lookup Item Frequency
-		outStream << "Please enter item to lookup:" << endl; 
+		outStream << BOLDBLUE;
+		outStream << "Please enter item to lookup:" << endl;
+		outStream << BOLDWHITE;
 		{
 			string input = getWord(inStream); // call helper to get input 
 			// call Python function to find item and return frequency, if not found frequency will be -1
@@ -39,9 +46,11 @@ void handleOption(char option, istream& inStream, ostream& outStream) {
 		displayFileHistogram("Frequency.dat", outStream); // Read File to display Histogram
 		break;
 	case '4': // quit
+		outStream << YELLOW;
 		outStream << "Exiting application" << endl;
 		break;
 	default: // unknown option
+		outStream << BOLDRED;
 		outStream << "Unknown option: Expected 1, 2, 3, or 4." << endl;
 		break;
 	}
@@ -79,10 +88,12 @@ void displayFileHistogram(string fileName, ostream& outStream) {
 	int frequency;
 	const char pattern = '*'; // histogram character
 
-	outStream << setw(15);
+	outStream << RESET;
+	outStream << BOLDWHITE;
 
 	inputFile.open(fileName);
 	if (!inputFile.is_open()) {
+		outStream << BOLDRED;
 		cout << "Could not open file " << fileName << "." << endl;
 		return;
 	}
@@ -94,7 +105,6 @@ void displayFileHistogram(string fileName, ostream& outStream) {
 		// need to check again otherwise last item prints twice
 		if (!inputFile.fail() && !inputFile.eof()) { // I think because the newline isn't consumed by the >> above so it loops one more time
 			string histogram = string(frequency, pattern);
-
 			outStream << right << setw(15) << item << " " << setw(64) << left << histogram << endl;
 		}
 	}
